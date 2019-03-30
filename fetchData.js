9e4a73e986406a69025fb6b6868cc2876b54cf03
Venugopal <gopal.venu113@gmail.com>
2019-03-30 11:14:01 +0530

    deleted fetchData

--- fetchData.js
+++
@@ -1,2 +0,0 @@
-const fetch = require('node-fetch');
-fetch('./JSON files/matchesTotal.json').then(res => res.json()).then(json => console.log(json))
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const fetch = require('node-fetch');
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    fetch('./JSON files/matchesTotal.json').then(res => res.json()).then(json => console.log(json))
+    fetch('https://github.com/gopalvenu113/ProjectIPL/blob/master/JSON%20files/matchesTotal.json').then(res => res.json()).then(json => console.log(json))
 }).listen(8080);
 console.log('server started')
